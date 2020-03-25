4b396027a19005d28ce64e139480d73471888bba
stefanT9 <stefan.tomsa99@gmail.com>
2020-03-25 11:58:56 +0200

    added some placeholder data to the graphs

---
+++ UI/statistics.js
@@ -0,0 +1,42 @@
+var charts = ['chart1', 'chart2', 'chart3', 'chart4', 'chart5', 'chart6']
+for (var ch in charts) {
+    console.log(ch);
+    var ctx = document.getElementById(charts[ch]).getContext('2d');
+    console.log(ctx);
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Italia', 'Franta', 'Romania', 'Japonia', 'China', 'Taiwan'],
+            datasets: [{
+                label: `scores in year ${2010+parseInt(ch)}`,
+                data: [Math.floor(300+Math.random()*300),Math.floor(300+Math.random()*300),Math.floor(300+Math.random()*300),Math.floor(300+Math.random()*300),Math.floor(300+Math.random()*300),Math.floor(300+Math.random()*300)],
+                backgroundColor: [
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                    'rgba(100, 100, 100, 0.2)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
