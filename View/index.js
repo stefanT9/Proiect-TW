9ba00a26af1eb6afed033fdaf76ae6a921147f48
stefanT9 <stefan.tomsa99@gmail.com>
2020-03-26 18:16:25 +0200

    Renamed files in index

---
+++ View/index.js
@@ -0,0 +1,9 @@
+function makeResponsive() {
+    var x = document.getElementById("TopNavId");
+
+    if (x.className === "topNav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topNav";
+    }
+}
