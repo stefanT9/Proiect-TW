4e01d7f558cffbefb6af77dc3b3ed59218367c82
rtudorica <rtudorica@bitdefender.com>
2020-04-22 20:02:10 +0300

    config pentru mongodb

--- utils/WebApp.js
+++ utils/WebApp.js
@@ -1,5 +1,9 @@
 const http = require('http')
 const { port } = require('../utils/constants')
+const { db_url } = require('../utils/constants')
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(db_url, { useUnifiedTopology: true });
+
 class WebApp {
   constructor (port, router) {
     this.port = port
@@ -12,6 +16,14 @@ class WebApp {
 
   listen () {
     var app = this
+
+    //e doar un proof of concept, fa conexiunea doar cand ai nevoie sa umblii la db
+    try{
+	client.connect();
+    } catch (e) {
+        console.error(e);
+    }
+
     var server = http.createServer(function (req, res) {
       app.router.route(req, res)
       res.end()
--- utils/constants.js
+++ utils/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
-  port: 3000
+  port: 3000,
+  db_url: "mongodb://aplicatieTW:5d036300f7b2585dcb8ec062951e9da1@centos-uni.zicar.info:27017/proiectTW_DB"
 }
