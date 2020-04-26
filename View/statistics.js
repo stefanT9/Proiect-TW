c3f449a285c7f82c20823e5884cb47151e71de24
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-04-26 16:10:00 +0300

    Created a custom element that takes care of a representation

--- View/statistics.js
+++ View/statistics.js
@@ -1,37 +1,67 @@
 let availableFields = [];
+let chartsPallete=['#003f5c', '#2f4b7c', '#665191', '#a05195', '#d45087', '#f95d6a', '#ff7c43' ,'#ffa600']
 
 function getGraphController (chartCanvas) {
   const graphController = document.createElement('div');
   const buttonsWrapper = document.createElement('div');
-  const deleteButton = document.createElement('button');
-  const exportAsCSV = document.createElement('button');
-  const exportAsJPG = document.createElement('button');
-  const exportAsPNG = document.createElement('button');
+
+  const deleteButton = document.createElement('a');
+  const exportAsCSV = document.createElement('a');
+  const exportAsJPG = document.createElement('a');
+  const exportAsPNG = document.createElement('a');
 
   const graphContainer = document.createElement('div');
-
-  buttonsWrapper.classList.add('graph-controller-button-wrapper')
 
   buttonsWrapper.appendChild(deleteButton);
   buttonsWrapper.appendChild(exportAsCSV);
   buttonsWrapper.appendChild(exportAsJPG);
   buttonsWrapper.appendChild(exportAsPNG);
 
+  deleteButton.classList.add('controller-button')
+  exportAsCSV.classList.add('controller-button')
+  exportAsJPG.classList.add('controller-button')
+  exportAsPNG.classList.add('controller-button')
+  
+  deleteButton.href='#'
+  exportAsCSV.href='#'
+  exportAsJPG.href='#'
+  exportAsPNG.href='#'
+
   graphController.appendChild(buttonsWrapper);
   graphController.appendChild(graphContainer);
   graphContainer.appendChild(chartCanvas);
 
-  
 
+  buttonsWrapper.classList.add('graph-controller-button-wrapper')
   graphContainer.classList.add('graphContainer');
   graphController.classList.add('graphController');
+
 
   deleteButton.innerText = 'remove graph';
   exportAsJPG.innerText = 'JPG'
   exportAsCSV.innerText = 'CSV'
   exportAsPNG.innerText = 'PNG'
 
-  deleteButton.onclick = () => { console.log('not implemented') };
+  deleteButton.onclick = () => { 
+    const wrapper=deleteButton.parentElement.parentElement
+    const body= wrapper.parentElement
+    body.removeChild(wrapper)
+  };
+
+  exportAsCSV.download='data.csv'
+  exportAsCSV.onclick = () =>{
+    console.log('not implemented')
+  }
+
+  exportAsJPG.download='grafic.jpg'
+  exportAsJPG.onclick = () => {
+    exportAsJPG.href = chartCanvas.toDataURL("image/jpg")
+  }
+
+  exportAsJPG.download='grafic.png'
+  exportAsPNG.onclick = () => {
+    exportAsJPG.href = chartCanvas.toDataURL("image/png")
+  }
 
   return graphController
 }
@@ -43,7 +73,7 @@ async function getAvailableFields () {
         return data.json()
       })
       .then(res => {
-        return res
+        return res  
       })
       .catch(err => {
         return undefined
@@ -52,39 +82,47 @@ async function getAvailableFields () {
 
 function closePopUp () {
   document.getElementById('invisibleBackground').classList.remove('grayout');
-  document.getElementById('fieldsPopUpForm').classList.remove('visible');
+  document.getElementById('popUpForm').classList.remove('visible');
   document.body.style.overflow = ''
+  document.getElementsByClassName('floatingButton')[0].classList.remove('invisible')
 }
 function addNewChart () {
-  const xLabel = 'testX';
-  const yLabel = 'testY';
-  const chartType = 'line';
+  const xLabel = document.getElementById('xOfGraph').value
+  const yLabel = document.getElementById('yOfGraph').value
+  const chartType = document.getElementById('typeOfGraph').value;
+
+  const xValues = ['test1','test2','test3','test4','test5','test6','test7','test8','test9','test10','test11']
   const dataArray = [Math.floor(300 + Math.random() * 300), Math.floor(300 + Math.random() * 300), Math.floor(300 + Math.random() * 300), Math.floor(300 + Math.random() * 300), Math.floor(300 + Math.random() * 300), Math.floor(300 + Math.random() * 300)];
   const chartCanvas = document.createElement('canvas');
 
   const myChart = new Chart(chartCanvas.getContext('2d'), {
     type: chartType,
     data: {
-      label: xLabel,
+      labels: xValues,
       datasets: [{
-        label: 'dataset1',
+        label: yLabel,
         data: dataArray,
-        backgroundColor: 'rgba(143, 200, 100, 0.2)',
-        borderColor: 'rgba(143, 200, 100, 0.2)',
+        backgroundColor: chartsPallete,
+        borderColor: chartsPallete,
         fill: false
     },]
     },
     options: {
       responsive:true,
+      maintainAspectRatio: false,
       title:{
           display:true,
-          text: `${xLabel} on ${yLabel} using ${chartType} representation`,
+          text: `x on y using ${chartType} representation`,
           fontColor: "#333"
       },
       scales: {
+        scaleLabel:{
+          display:true,
+          labelString: 'test123'
+        },
         yAxes: [{
           ticks: {
-            beginAtZero: true
+            beginAtZero: false
           }
         }]
       }
@@ -96,8 +134,10 @@ function addNewChart () {
 }
 function openPopUp () {
   document.getElementById('invisibleBackground').classList.add('grayout');
-  document.getElementById('fieldsPopUpForm').classList.add('visible');
+  document.getElementById('popUpForm').classList.add('visible');
   document.body.style.overflow = 'hidden'
+  document.getElementsByClassName('floatingButton')[0].classList.add('invisible')
+
 }
 
 function loadFields () {
