230a0de95b13bdcc34ce98819c72633d180b1cf4
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-05-14 15:47:47 +0300

    corrected redirects

--- routes/pages.js
+++ routes/pages.js
@@ -3,21 +3,21 @@ const { hostUrl } = require('../utils/constants')
 const router = new Router()
 
 router.get('/', async (req, res) =>{
-    res.writeHead(302, {'Location': hostUrl + 'index.html'});
+    res.writeHead(302, {'Location': hostUrl + '/index.html'});
     res.end();
 })
 
 router.get('/index', async (req, res) =>{
-    res.writeHead(302, {'Location': hostUrl + 'index.html'});
+    res.writeHead(302, {'Location': hostUrl + '/index.html'});
     res.end();
 })
 
 router.get('/statistics', async (req, res) => {
-    res.writeHead(302, {'Location': hostUrl + 'statistics.html'});
+    res.writeHead(302, {'Location': hostUrl + '/statistics.html'});
     res.end();
 })
 router.get('/results', async (req, res) =>{
-    res.writeHead(302, {'Location': hostUrl + 'results.html'});
+    res.writeHead(302, {'Location': hostUrl + '/results.html'});
     res.end();
 })
 
