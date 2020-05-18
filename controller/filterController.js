dd33438294f595033e2637bf080f259450c1e005
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-05-18 21:53:45 +0300

    added missing error handling

--- controller/filterController.js
+++ controller/filterController.js
@@ -1,26 +1,33 @@
 module.exports.getColumns = async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   req.db.Columns.find({}, (err, columns) => {
-    var results = []
-    for (var i = 0; i < columns.length; i++) {
-      var columnFound = { name: columns[i].name, details: columns[i].details, type: columns[i].type }
-      if (columnFound.type == 'discrete') {
-        columnFound.values = []
-        for (value of columns[i].translate) {
-          columnFound.values.push(value[1])
+    if (err) {
+      console.log(err)
+      res.statusCode = 500
+      res.write(JSON.stringify({ success: false, message: 'Could not fetch data' }))
+      res.end()
+    } else {
+      var results = []
+      for (var i = 0; i < columns.length; i++) {
+        var columnFound = { name: columns[i].name, details: columns[i].details, type: columns[i].type }
+        if (columnFound.type === 'discrete') {
+          columnFound.values = []
+          for (const value of columns[i].translate) {
+            columnFound.values.push(value[1])
+          }
+        } else {
+          if (columns[i].min === null || columns[i].max === null) {
+            continue
+          }
+          columnFound.min = columns[i].min
+          columnFound.max = columns[i].max
         }
-      } else {
-        if (columns[i].min === null || columns[i].max === null) {
-          continue
-        }
-        columnFound.min = columns[i].min
-        columnFound.max = columns[i].max
+        results.push(columnFound)
       }
-      results.push(columnFound)
+      res.statusCode = 200
+      res.write(JSON.stringify({ success: true, columns: results }))
+      res.end()
     }
-    res.statusCode = 200
-    res.write(JSON.stringify({ success: true, columns: results }))
-    res.end()
   })
 }
 
