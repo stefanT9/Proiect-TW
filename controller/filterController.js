21567167943124399a13fc1ae69a55263bba0312
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-05-19 18:17:05 +0300

    moved get column route

--- controller/filterController.js
+++ controller/filterController.js
@@ -1,36 +1,3 @@
-module.exports.getColumns = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  req.db.Columns.find({}, (err, columns) => {
-    if (err) {
-      console.log(err)
-      res.statusCode = 500
-      res.write(JSON.stringify({ success: false, message: 'Could not fetch data' }))
-      res.end()
-    } else {
-      var results = []
-      for (var i = 0; i < columns.length; i++) {
-        var columnFound = { name: columns[i].name, details: columns[i].details, type: columns[i].type }
-        if (columnFound.type === 'discrete') {
-          columnFound.values = []
-          for (const value of columns[i].translate) {
-            columnFound.values.push(value[1])
-          }
-        } else {
-          if (columns[i].min === null || columns[i].max === null) {
-            continue
-          }
-          columnFound.min = columns[i].min
-          columnFound.max = columns[i].max
-        }
-        results.push(columnFound)
-      }
-      res.statusCode = 200
-      res.write(JSON.stringify({ success: true, columns: results }))
-      res.end()
-    }
-  })
-}
-
 module.exports.filterResults = async (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   try {
