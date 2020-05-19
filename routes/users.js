1db7f792657986a99e05b3149a299a18d03f164f
stefanT9 <46680707+stefanT9@users.noreply.github.com>
2020-05-20 00:06:01 +0300

    fixed bug where regex conflicted with non regex route

--- routes/users.js
+++ routes/users.js
@@ -9,18 +9,21 @@ router.get('/', async (req, res) => {
 	res.end()
 })
 
-router.get('/users', userController.getAll)
-router.get('/users/:id', userController.getFunction)
-router.post('/users/', userController.postFunction)
-router.delete('/users/:id', userController.deleteFunction)
-router.put('/users/:id', userController.putFunction)
-
 router.get('/auth', async (req, res) => {
   res.writeHead(200, {})
   res.write('service is up')
   res.end()
 })
+
+router.get('/users', userController.getAll)
+router.post('/users/', userController.postFunction)
 router.post('/auth/login', authController.login)
 router.post('/auth/register', authController.register)
+
+
+router.delete('/users/:id', userController.deleteFunction)
+router.put('/users/:id', userController.putFunction)
+router.get('/users/:id', userController.getFunction)
+
 
 module.exports.usersRouter = router
--- routes/values.js
+++ routes/values.js
@@ -3,10 +3,11 @@ const { Router } = require('../utils/Router')
 const router = new Router()
 
 router.get('/values', valueController.getAll)
-router.get('/values/:id', valueController.getFunction)
 router.post('/values/', valueController.postFunction)
 router.post('/values/insert', valueController.insert)
 router.post('/values/find', valueController.filterResults)
+
+router.get('/values/:id', valueController.getFunction)
 router.put('/values/:id', valueController.putFunction)
 router.delete('/values/:id', valueController.deleteFunction)
 
