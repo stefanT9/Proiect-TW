c584dddfc2ce6f371c6fb78fcc9f08fe6bef175d
stefanT99 <stefan.tomsa99@gmail.com>
2020-06-10 21:21:24 +0300

    Fixed isAuthRoutes

--- routes/values.js
+++ routes/values.js
@@ -2,13 +2,14 @@ const valueController = require('../controller/valuesController')
 const { Router } = require('../utils/Router')
 const router = new Router()
 
-router.get('/values', valueController.getAll)
-router.post('/values/', valueController.postFunction)
-router.post('/values/insert', valueController.insert)
 router.post('/values/find', valueController.filterResults)
 
-router.get('/values/:id', valueController.getFunction)
-router.put('/values/:id', valueController.putFunction)
-router.delete('/values/:id', valueController.deleteFunction)
+router.get('/administrative/values', valueController.getAll)
+router.post('/administrative/values/insert', valueController.insert)
+
+router.get('/administrative/values/:id', valueController.getFunction)
+router.post('/administrative/values/', valueController.postFunction)
+router.put('/administrative/values/:id', valueController.putFunction)
+router.delete('/administrative/values/:id', valueController.deleteFunction)
 
 module.exports.valuesRouter = router
